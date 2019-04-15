cba19284e3ad3825b710388caef344c06ab95aaf
ALTRAN_ES\alberto.quiros <alberto.munoz@altran.com>
2019-04-15 22:29:21 +0200

    Strings related refactor

--- app/src/main/java/com/amqo/randomuser/data/network/ApiSecret.kt
+++ app/src/main/java/com/amqo/randomuser/data/network/ApiSecret.kt
@@ -2,5 +2,6 @@ package com.amqo.randomuser.data.network
 
 object ApiSecret {
 
-    const val GOOGLE_MAPS_API_KEY = "AIzaSyDdAVZuR18LjK1edIDx5rwvhP4Oh3FbraE"
+    //  Please provide your own apiKey to make static maps work
+    const val GOOGLE_MAPS_API_KEY = ""
 }
